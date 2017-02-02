bf446d92257706eb4f923ac437025d5f07757b9b
Emma Hou <emma.houy@gmail.com>
2017-02-02 15:04:28 -0500

    Fix players router

--- routes/players.js
+++ routes/players.js
@@ -1,16 +1,16 @@
 'use strict'
 
 const express     = require('express');
-const users       = express.Router();
+const players     = express.Router();
 const bodyParser  = require('body-parser');
 const db          = require('./../db/players');
 
-users.use(function(error, request, response, next) {
+players.use(function(error, request, response, next) {
   if(error.name === 'UnauthorizredError') {
     response.status(401).json({message: 'you need an authoriation token to view condifential information'});
   }
 });
 
-users.route('/')
+players.route('/')
   .get( db.allPlayers )
   .post( db.createPlayer )
